42dc0ead78844eed24d3f98c38e256b351ff7868
ArunKumarSINCE1997 <urstrulyarunkumar9309@gmail.com>
2022-02-19 05:32:37 +0530

    third project added

---
+++ PROGRAM3/src/mypackage/MyClass.java
@@ -0,0 +1,15 @@
+package mypackage;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		int num = 11;
+		if(num%2==0)
+			System.out.println("it is an even number");
+		else
+			System.out.println("it is an odd number");
+			
+
+	}
+
+}
